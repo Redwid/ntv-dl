1d70aa64c452172123ce3bda04fa5ee8b70e2801
Redwid <redwid@gmail.com>
2019-03-15 15:31:22 +0000

    First commit

---
+++ ntv-dl.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python -*- coding: utf-8 -*-
+import requests
+import subprocess
+from datetime import datetime
+from dataclasses import dataclass
+from operator import attrgetter
+
+NTV_EDA_JIVAYA_I_MERTVAYA_JSON_URL = 'http://www.ntv.ru/m/v10/prog/Eda_jivaya_i_mertvaya/'
+NTV_PEREDELKA_JSON_URL = 'http://www.ntv.ru/m/v10/prog/peredelka/'
+NTV_DACHA_OTVET_JSON_URL = 'http://www.ntv.ru/m/v10/prog/dacha_otvet/'
+NTV_CLIENT_USER_AGENT = 'ru.ntv.client_v4.9'
+HEADERS = {'User-Agent': NTV_CLIENT_USER_AGENT}
+
+@dataclass
+class VideoItem(object):
+    id: int
+    ms: int
+    title: str
+    url: str
+    hiVideoUrl: str
+    loVideoUrl: str
+
+
+def downloadJson(jsonUrl):
+    print('downloadJson(), jsonUrl: ', jsonUrl)
+    resp = requests.get(url = jsonUrl, headers = HEADERS)
+    data = resp.json()
+    menus = data['data']['menus']
+
+    videoItemList = []
+    for item in menus:
+        ms = 0
+        sharelink = ''
+        hi_video = ''
+        lo_video = ''
+        if 'issues' in item['data']:
+            issues = item['data']['issues']
+            for issue in issues:
+                id = issue['id']
+                title = issue['title']
+                if 'video_list' in issue:
+                    video_list = issue['video_list']
+                    for video in video_list:
+                        ms = video['ts']
+                        #print(datetime.utcfromtimestamp(ms//1000).replace(microsecond=ms%1000*1000))
+                        sharelink = video['sharelink']
+                        hi_video = video['hi_video']
+                        lo_video = video['video']
+                videoItem = VideoItem(id, ms, title, sharelink, hi_video, lo_video)
+                videoItemList.append(videoItem)
+    return videoItemList
+
+def getVideoUrl(videoItem):
+    urls = [videoItem.hiVideoUrl, videoItem.loVideoUrl]
+    for url in urls:
+        print('head for url: ', url)
+        r = requests.head(url, headers = HEADERS)
+        print('r.status_code: ', r.status_code)
+        if r.status_code == 200:
+            return videoItem.hiVideoUrl
+    return None
+
+if __name__ == '__main__':
+    print('main')
+    urls = [NTV_EDA_JIVAYA_I_MERTVAYA_JSON_URL, NTV_PEREDELKA_JSON_URL, NTV_DACHA_OTVET_JSON_URL]
+    for url in urls:
+        print('url: ', url)
+        videoItemList = downloadJson(url)
+        videoItemList.sort(key = attrgetter('ms'), reverse = False)
+        videoItem = videoItemList[-1]
+        print('To download:', videoItem)
+        url = getVideoUrl(videoItem)
+        print('Url to download:', url)
+        if url is not None:
+            print('Url start download:', url)
+            subprocess.run(['wget', '-N', '-U', NTV_CLIENT_USER_AGENT, '-O', videoItem.title + '.mp4', url])
+
