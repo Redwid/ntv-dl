8225365e03657a450b928d0bea544d5cb9d7039b
Redwid <redwid@gmail.com>
2020-09-05 12:38:28 +0100

    Disable engine debug output

--- ntv-dl.py
+++ ntv-dl.py
@@ -381,7 +381,7 @@ def get_value(item, key):
 #
 def get_db_session(db_user_name, db_user_password):
     url = "mysql+mysqlconnector://{}:{}@192.168.1.29:3306/ntv".format(db_user_name, db_user_password)
-    engine = sal.create_engine(url, echo = True)
+    engine = sal.create_engine(url, echo = False)
     connection = engine.connect()
     metadata = sal.MetaData()
 
